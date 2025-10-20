35c51e9be0f33b78702e7e1767d81c2f3fb49ce9
Jasper Lin <73963814+JasperLin0118@users.noreply.github.com>
2025-10-20 21:24:02 +0800

    [HRSM-6] update item and package response Dto (#6)
    * [HRSM-6] update item and package response Dto
    
    * [HRSM-6] delete datetime

--- HRS.Shared.Core/Dtos/PackageDto.cs
+++ HRS.Shared.Core/Dtos/PackageDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace HRS.Shared.Core.Dtos;
@@ -5,14 +6,14 @@ namespace HRS.Shared.Core.Dtos;
 
 public class PackageItemResponseDto
 {
-    public int ItemId { get; set; }
+    public string ItemId { get; set; } = string.Empty;
     public string ItemName { get; set; } = string.Empty;
     public int Quantity { get; set; }
 }
 
 public class PackageRateResponseDto
 {
-    public int Id { get; set; }
+    public string Id { get; set; } = string.Empty;
     public int MinDays { get; set; }
     public decimal DailyRate { get; set; }
     public bool IsActive { get; set; }
@@ -20,8 +21,9 @@ public class PackageRateResponseDto
 
 public class PackageResponseDto
 {
-    public int Id { get; set; }
+    public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public string StoreId { get; set; } = string.Empty;
     public string? Description { get; set; }
     public decimal BasePrice { get; set; }
 
