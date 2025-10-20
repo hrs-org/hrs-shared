6586a123b8528a3054df39cadf2758543cff02d4
feri-nus-iss <e1546606@u.nus.edu>
2025-10-20 14:53:19 +0800

    [HRSM-7] Item Maintenance Shared (#3)
    * [HRSM-7] Item Maintenance Shared
    
    * Fix Warning

---
+++ HRS.Shared.Core/Dtos/ItemMaintenanceDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using HRS.Shared.Core.Enums;
+
+namespace HRS.Shared.Core.Dtos;
+#nullable enable
+
+public sealed class CreateItemMaintenanceRequestDto
+{
+    [Required] public required string ItemId { get; set; }
+    [Required] public required string RentalOrderId { get; set; }
+    public ItemMaintenanceType Type { get; set; }
+    public int Quantity { get; set; }
+    public int CreatedById { get; set; }
+    public string? Remarks { get; set; }
+}
+
+public class FixItemMaintenanceRequestDto
+{
+    [Required] public string Id { get; set; } = string.Empty;
+    [Required] public int QuantityFixed { get; set; }
+    public string? Remarks { get; set; }
+}
+
+public class ItemMaintenanceResponseDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string ItemId { get; set; } = string.Empty;
+    public string RentalOrderId { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public int? QuantityFixed { get; set; }
+    public string Remarks { get; set; } = string.Empty;
+}
