1d330ee98c7de5c22c7be300fa2725a6746f00b2
feri-nus-iss <e1546606@u.nus.edu>
2025-10-20 18:22:55 +0800

    [HRSM-15] store id in response

--- HRS.Shared.Core/Dtos/UserDto.cs
+++ HRS.Shared.Core/Dtos/UserDto.cs
@@ -3,6 +3,7 @@ namespace HRS.Shared.Core.Dtos;
 public class UserResponseDto
 {
     public int Id { get; set; }
+    public int StoreId { get; set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
