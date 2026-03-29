8619dd5ae5eea689c25d1819e6ff9012967320e3
feri-nus-iss <e1546606@u.nus.edu>
2026-03-29 15:26:30 +0800

    feat: add scope-based authorization handlers for Auth0
    Add shared ScopeRequirement and ScopeHandler classes to enable
    scope-based authorization across microservices using Auth0 scopes.

---
+++ HRS.Shared.Core/Authorization/ScopeHandler.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace HRS.Shared.Core.Authorization;
+
+public class ScopeHandler : AuthorizationHandler<ScopeRequirement>
+{
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        ScopeRequirement requirement)
+    {
+        var scopeClaim = context.User.FindFirst(c =>
+            c.Type == "scope" &&
+            c.Issuer == requirement.Issuer);
+
+        if (scopeClaim == null)
+            return Task.CompletedTask;
+
+        var scopes = scopeClaim.Value.Split(' ');
+
+        if (scopes.Any(s =>
+                s == requirement.Scope))
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ HRS.Shared.Core/Authorization/ScopeRequirement.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace HRS.Shared.Core.Authorization;
+
+public class ScopeRequirement : IAuthorizationRequirement
+{
+    public string Scope { get; set; }
+    public string Issuer { get; set; }
+
+    public ScopeRequirement(string scope, string issuer)
+    {
+        Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        Issuer = $"https://{issuer}/";
+    }
+}
